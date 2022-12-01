25714a1a71236e5cc8f2a100a09e250fb85a3fe8
zaferguvenn <zafer.guven58@gmail.com>
2022-12-01 17:56:25 +0300

    İleri Fonksiyonlar

--- app/src/main/java/com/example/leriseviyefonksiyonlar/İleriSeviyeFonksiyonlar2.kt
+++ app/src/main/java/com/example/leriseviyefonksiyonlar/İleriSeviyeFonksiyonlar2.kt
@@ -1,3 +1,53 @@
 package com.example.leriseviyefonksiyonlar
 
 //soon..
+
+fun main(){
+
+    /*
+    Let
+     */
+    //null kontrolü ve not null yapabilme özelliği var.
+
+    //gosterim I
+    val myInt :Int?=null
+
+
+    myInt?.let{//myInt null değil ise aşağıdakini yap
+        val num=it+1
+        println(num)//bir şey yazmaz çünkü myInt null
+    }
+
+    //gosterim II
+    val myInt2:Int?=null
+
+    val num2=myInt2?.let {//null olduğundan it+1 işlemini yapmaz.
+        it+1
+    }?:0
+
+    println(num2)//0 yazar çünkü yukarıda "?:0" yazarak default değeri 0 yaptık.
+
+
+    /*
+    Also
+     */
+    //ayrıca bunu yap anlamına gelir
+
+
+    val book1= Book("Yeraltından Notlar",97)
+    val book2= Book("Kürk Mantolu Madonna", 177)
+    val book3= Book("Sızma Sanatı", 300)
+
+    val Library= listOf<Book>(book1,book2,book3)
+
+    Library.filter{ it.page < 200 }.also { it.forEach()//200 sayfadan küçük olanları filtrele
+                                                        //ayrıca isimleri yazdır.
+        {
+         println(it.name)//Yeraltından Notlar
+                        //Kürk Mantolu Madonna  yazar
+        }
+    }
+
+}
+
+data class Book(var name: String, val page:Int)
---
+++ app/src/main/java/com/example/leriseviyefonksiyonlar/İleriSeviyeFonksiyonlar3.kt
@@ -0,0 +1,55 @@
+package com.example.leriseviyefonksiyonlar
+
+
+
+fun main(){
+
+    /*
+    * Apply
+    * */
+    //Tekrar edilen durumlarda kullanılabilir.
+
+    //normal gösterim
+    val tekrar= Tekrar()
+
+    tekrar.topla()
+    tekrar.carp()
+
+    //apply kullanarak
+    val tekrar2=Tekrar().apply {
+        topla()
+        carp()
+    }
+
+
+    /*
+    * With
+    * */
+    //apply'a benzer ancak bazen bir şey döndürmez.
+
+    val emtyBook=with(Book("efiller",543)){
+        name="Sefiller"
+    }
+    println(emtyBook)//Unit yazar. Apply yazsaydık çıktı Sefiller olurdu.
+
+
+
+    val notEmtyBook=Book("efiller",543)
+
+    with(notEmtyBook){
+        name="Sefiller"
+    }
+    println(notEmtyBook)//Book(name=Sefiller, page=543)
+}
+
+class Tekrar(){
+
+    fun topla(){
+        println(3+4)
+    }
+
+
+    fun carp(){
+        println(3*5)
+    }
+}
